5e78288b275244d803d2bd1fd494dd787aed75c5
JR <1199377+jresteban@users.noreply.github.com>
2024-08-21 17:37:38 +0000

    creación de páginas

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+
+  <header class="p-4 bg-gray-800 text-white flex flex-row justify-between items-center">
+    <h1 class="text-4xl">My App</h1>
+    <nav>
+      <NuxtLink to="/" class="mr-4">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </nav>
+  </header>
+  <main class="p-4">
+    <NuxtPage />
+  </main>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h1>Home</h1>
+    <p>{{ message }}</p>
+  </section>
+</template>
+
+<script setup lang="ts">
+const message = 'Hello, world!'
+</script>
