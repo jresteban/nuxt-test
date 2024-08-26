092cddd8d1f1116cbdd030a4fb0834914bb9a24c
JR <1199377+jresteban@users.noreply.github.com>
2024-08-26 19:46:41 +0000

    switch mode component

--- app.vue
+++ app.vue
@@ -1,11 +1,16 @@
+<script setup lang="ts">
+import SwitchMode from './components/switchMode.vue';
+</script>
+
 <template>
   <div class="h-screen">
-  <header class="p-4 bg-gray-800 text-white flex flex-row justify-between items-center">
-    <h1 class="text-4xl">My App</h1>
+  <header class="p-4 bg-gray-800 text-white flex flex-row justify-between items-center gap-4">
+    <h1 class="text-4xl flex-1">My App</h1>
     <nav>
       <NuxtLink to="/" class="mr-4">Home</NuxtLink>
       <NuxtLink to="/linkShortener">Link Shortener</NuxtLink>
     </nav>
+    <SwitchMode />
   </header>
   <main class="p-4 h-full">
     <NuxtPage />
---
+++ components/switchMode.vue
@@ -0,0 +1,13 @@
+<template>
+  <UToggle v-model="isDarkMode" on-icon="i-heroicons-moon-16-solid" off-icon="i-heroicons-sun-16-solid" />
+</template>
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const isDarkMode = computed({
+  get: () => colorMode.value === 'dark',
+  set: (value: boolean) => {
+    colorMode.preference = value ? 'dark' : 'light'
+  }
+})
+</script>
